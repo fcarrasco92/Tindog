006dc54c6a95cfcdfd73eb144a22ef3fb33ef895
Felipe <carrascogalindo.felipe@gmail.com>
2018-08-26 19:34:15 -0300

    [add] Splash Screen View

--- Tindog/Controller/HomeViewController.swift
+++ Tindog/Controller/HomeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RevealingSplashView
 
 class NavigationImageView : UIImageView {
     override func sizeThatFits(_ size: CGSize) -> CGSize {
@@ -19,10 +20,13 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var homeWrapper: UIStackView!
     @IBOutlet weak var likeImage: UIImageView!
     @IBOutlet weak var nopeImage: UIImageView!
+    let revealingSplashScreen = RevealingSplashView(iconImage: UIImage(named: "splash_icon")!, iconInitialSize: CGSize(width: 80, height : 80), backgroundColor: UIColor.white)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.addSubview(self.revealingSplashScreen)
+        self.revealingSplashScreen.animationType = SplashAnimationType.popAndZoomOut
+        self.revealingSplashScreen.startAnimation()
         let titleView = NavigationImageView()
         titleView.image = UIImage(named: "Actions")
         self.navigationItem.titleView = titleView
